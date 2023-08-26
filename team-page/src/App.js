95222d54d60e9eb80a913a8ae5e2b06f84e80708
Chirag0606 <thakur.chirag17@yahoo.com>
2023-08-27 03:12:03 +0530

    remaining files added

--- team-page/src/App.js
+++ team-page/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Components/Navbar';
+import Uni from './Components/Uni';
+import Mentors from './Components/Mentors';
+import Advisor from './Components/Advisor';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Uni />
+      <Mentors />
+      <Advisor />
+      <Footer />
+    </>
   );
 }
 
